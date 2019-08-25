b38c267eca833bf640e8ebf0a605af2f7875588d
ivorzk <ivorzk@qq.com>
2019-08-26 00:27:29 +0800

    调整目录结构！

--- app/src/main.js
+++ app/src/main.js
@@ -1,57 +1,24 @@
-const { app, BrowserWindow } = require('electron');
-
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
+import Vue from 'vue'
+import store from './store'
+// import './registerServiceWorker'
+// 注入全局组件
+import './components'
+// 注入全局样式
+import './styles/index.scss'
+import config from './../app.config'
+Vue.config.productionTip = false
+Vue.prototype.$config = config
+// 动态获取url映射到组件
+let path = location.pathname
+path = path.substring(path.lastIndexOf('/') + 1, path.lastIndexOf('.html') || path.length)
+if (path == '/') path = 'index'
+let page
+try {
+  page = require(`./pages/${path}`).default
+} catch (e) {
+  page = require(`./pages/index`).default
 }
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
-
-const createWindow = () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
-
-  // and load the index.html of the app.
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-};
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+window.$vm = new Vue({
+  store,
+  render: h => h(page),
+}).$mount('#app')
