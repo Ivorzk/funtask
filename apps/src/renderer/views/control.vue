d3fe0484301bdd78fb474e1e896230c38cb1e481
赵成围 <ivorzk@qq.com>
2019-09-25 21:45:16 +0800

    v

--- apps/src/renderer/views/control.vue
+++ apps/src/renderer/views/control.vue
@@ -1,6 +1,13 @@
 <template>
 <div class="suwis-control">
-  你好
+  <div class="fun-list">
+    <ul>
+      <li v-for="item in 15">
+        <i class="iconfont">xxx</i>
+        功能
+      </li>
+    </ul>
+  </div>
 </div>
 </template>
 
@@ -11,9 +18,28 @@ export default {}
 <style lang="scss" scoped>
 .suwis-control {
     position: relative;
-    width: 98vw;
-    height: 98vh;
+    width: 100vw;
+    height: 100vh;
     background: rgba(0,0,0,0.8);
     border-radius: 10px;
+
+    .fun-list {
+        ul {
+            display: flex;
+            flex-wrap: wrap;
+            list-style: none;
+        }
+
+        li {
+            max-width: 20%;
+            min-width: 20%;
+            color: #fff;
+            text-align: center;
+            padding: 3% 0;
+            i {
+                display: block;
+            }
+        }
+    }
 }
 </style>
