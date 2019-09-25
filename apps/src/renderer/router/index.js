deff126c03670bd3ef96ead53aaaf1288909f6fe
赵成围 <ivorzk@qq.com>
2019-09-25 14:11:40 +0800

    调整项目！

--- apps/src/renderer/router/index.js
+++ apps/src/renderer/router/index.js
@@ -4,15 +4,12 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage').default
-    },
-    {
-      path: '*',
-      redirect: '/'
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'index',
+    component: require('@/views/index').default
+  }, {
+    path: '*',
+    redirect: '/'
+  }]
 })
