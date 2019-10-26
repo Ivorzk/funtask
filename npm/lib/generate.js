66be99f7b2a068a806fb019dbcd27b8b78fe592a
赵成围 <ivorzk@qq.com>
2019-10-26 14:50:10 +0800

    持续更改！

--- npm/lib/generate.js
+++ npm/lib/generate.js
@@ -1,8 +1,13 @@
-const fs = require('fs')
 const path = require('path')
 const gulp = require('gulp')
+const replace = require('gulp-replace')
 
 // export
-module.exports = (args) => {
-  console.log('generate')
+module.exports = (name) => {
+  console.log(process.execPath)
+  console.log(__dirname)
+  console.log(process.cwd())
+  // copy templates
+  gulp.src(`${__dirname}/../plugin-tpl/**/*`)
+    .pipe(gulp.dest(path.resolve(`./${name}/`)))
 }
