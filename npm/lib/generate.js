74141cb84a4953e70c51408d33ec21134af7c3b3
赵成围 <ivorzk@qq.com>
2019-10-26 15:54:01 +0800

    update v1.0.2

--- npm/lib/generate.js
+++ npm/lib/generate.js
@@ -1,13 +1,42 @@
 const path = require('path')
 const gulp = require('gulp')
 const replace = require('gulp-replace')
+const chalk = require('chalk')
+// tasker
+const Tasks = function() {}
+
+// options
+const options = {}
+
+// copy templates
+Tasks.prototype.copyTemplates = () => {
+  gulp.src([`${__dirname}/../plugin-tpl/**/*`, `!${__dirname}/../plugin-tpl/package.json`])
+    .pipe(gulp.dest(path.resolve(`./${options.name}/`)))
+}
+
+// replace variables
+Tasks.prototype.replaceVariables = () => {
+  gulp.src([`${__dirname}/../plugin-tpl/package.json`])
+    .pipe(replace('${name}', `@funtask/${options.name}`))
+    .pipe(replace('${description}', `a funtask app`))
+    .pipe(replace('${keywords}', `keywords`))
+    .pipe(replace('${repository}', ``))
+    .pipe(gulp.dest(path.resolve(`./${options.name}/`)))
+}
 
 // export
 module.exports = (name) => {
-  console.log(process.execPath)
-  console.log(__dirname)
-  console.log(process.cwd())
+  // init options
+  options.name = name
+
+  // create instance
+  var io = new Tasks()
+
   // copy templates
-  gulp.src(`${__dirname}/../plugin-tpl/**/*`)
-    .pipe(gulp.dest(path.resolve(`./${name}/`)))
+  io.copyTemplates()
+
+  // replace variables
+  io.replaceVariables()
+
+  console.log(chalk.yellow('Application built successfully !'))
 }
