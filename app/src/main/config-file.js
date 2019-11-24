f4ddaa6548aacf45cb6ad5a99432406936cbb45d
ivorzk <ivorzk@qq.com>
2019-11-25 02:32:08 +0800

    调整目录结构！

--- app/src/main/config-file.js
+++ app/src/main/config-file.js
@@ -1,5 +1,16 @@
 import YAML from 'yaml'
+import os from 'os'
+console.log(os.homeidr())
+class config {
+  // 获取配置文件目录
+  get configdir() {
 
-export default {
+  }
 
+  // 载入配置文件
+  loadConfig() {
+
+  }
 }
+
+export default new config()
--- app/src/main/index.js
+++ app/src/main/index.js
@@ -1,10 +1,6 @@
 'use strict'
-
-import {
-  app,
-  BrowserWindow,
-  ipcMain
-} from 'electron'
+import config from './config-file'
+import control from './control'
 
 /**
  * Set `__static` path to static files in production
@@ -13,124 +9,6 @@ import {
 if (process.env.NODE_ENV !== 'development') {
   global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
-
-// 小球
-let ballwin
-// 菜单窗口
-let controlwin
-
-// 入口页面
-const winURL = process.env.NODE_ENV === 'development' ? `http://localhost:9080` : `file://${__dirname}/index.html`
-
-// 创建窗体
-function createWindow() {
-  /**
-   * Initial window options
-   */
-  ballwin = new BrowserWindow({
-    width: 88,
-    height: 88,
-    frame: false,
-    transparent: true,
-    resizable: false,
-    maximizable: false,
-    minimizable: false,
-    alwaysOnTop: true,
-    fullscreenable: false,
-    hasShadow: false,
-    useContentSize: true,
-    skipTaskbar: true,
-    webPreferences: {
-      nodeIntegration: true,
-      nodeIntegrationInWorker: true
-    }
-  })
-
-  ballwin.loadURL(winURL)
-
-  ballwin.on('closed', () => {
-    ballwin = null
-  })
-
-  // 初始化菜单
-  controlwin = new BrowserWindow({
-    x: 0,
-    y: 0,
-    width: 618,
-    height: 380,
-    frame: false,
-    transparent: true,
-    resizable: false,
-    maximizable: false,
-    minimizable: false,
-    alwaysOnTop: true,
-    fullscreenable: false,
-    hasShadow: true,
-    skipTaskbar: true,
-    opacity: 0,
-    webPreferences: {
-      nodeIntegration: true,
-      nodeIntegrationInWorker: true
-    }
-  })
-
-  controlwin.loadURL(`${winURL}/#/control`)
-
-  controlwin.on('closed', () => {
-    controlwin = null
-  })
-}
-
-// 同步菜单位置
-function syncPosition(flag) {
-  // console.log(flag, 'flag')
-  // 小球位置
-  let ballpos = ballwin.getPosition()
-  // 窗体位置
-  let controlpos = controlwin.getPosition()
-  let controlBounds = controlwin.getBounds()
-  if (flag === 'control') {
-    controlwin.setPosition(ballpos[0] - controlBounds.width + 75, ballpos[1] - controlBounds.height + 75)
-  } else {
-    ballwin.setPosition(controlpos[0] + controlBounds.width - 75, controlpos[1] + controlBounds.height - 75)
-  }
-}
-
-app.on('ready', createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  if (ballwin === null) {
-    createWindow()
-  }
-})
-
-// 监听菜单状态改变
-ipcMain.on('control-toggle', (evt, args) => {
-  // 切换窗体类型
-  let wintype = controlwin.getOpacity() === 0 ? 'control' : 'ball'
-  // 同步菜单位置
-  syncPosition(wintype)
-  ballwin.webContents.send('control-reply', wintype === 'ball')
-  controlwin.webContents.send('control-reply', wintype === 'control')
-  // 设置显示和隐藏 间隔300毫秒，等待动画执行完成
-  if (wintype === 'control') {
-    controlwin.setOpacity(wintype === 'control' ? 1 : 0)
-    setTimeout(() => {
-      ballwin.setOpacity(wintype === 'ball' ? 1 : 0)
-    }, 300)
-  } else {
-    ballwin.setOpacity(wintype === 'ball' ? 1 : 0)
-    setTimeout(() => {
-      controlwin.setOpacity(wintype === 'control' ? 1 : 0)
-    }, 300)
-  }
-})
 
 /**
  * Auto Updater
