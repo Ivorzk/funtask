d79c63dc0843381761453c37cb0268b8bfb88d8b
ivorzk <ivorzk@qq.com>
2019-11-29 02:45:02 +0800

    持续更新！

--- app/src/main-process/control.js
+++ app/src/main-process/control.js
@@ -1,9 +1,23 @@
 'use strict'
 import {
   app,
+  protocol,
   BrowserWindow,
   ipcMain
 } from 'electron'
+import {
+  createProtocol,
+  installVueDevtools
+} from 'vue-cli-plugin-electron-builder/lib'
+const isDevelopment = process.env.NODE_ENV !== 'production'
+// Scheme must be registered before the app is ready
+protocol.registerSchemesAsPrivileged([{
+  scheme: 'app',
+  privileges: {
+    secure: true,
+    standard: true
+  }
+}])
 /**
  * 控制台
  */
@@ -12,11 +26,22 @@ export default class {
   ballwin = {}
   // 菜单窗口
   controlwin = {}
-  // 入口页面
-  winURL = process.env.NODE_ENV === 'development' ? `http://localhost:9080` : `file://${__dirname}/index.html`
   // 构造函数
   constructor() {
     app.on('ready', () => {
+      if (isDevelopment && !process.env.IS_TEST) {
+        // Install Vue Devtools
+        // Devtools extensions are broken in Electron 6.0.0 and greater
+        // See https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/378 for more info
+        // Electron will not launch with Devtools extensions installed on Windows 10 with dark mode
+        // If you are not using Windows 10 dark mode, you may uncomment these lines
+        // In addition, if the linked issue is closed, you can upgrade electron and uncomment these lines
+        // try {
+        //   await installVueDevtools()
+        // } catch (e) {
+        //   console.error('Vue Devtools failed to install:', e.toString())
+        // }
+      }
       this.createWindow()
     })
 
@@ -31,6 +56,21 @@ export default class {
         this.createWindow()
       }
     })
+
+    // Exit cleanly on request from parent process in development mode.
+    if (isDevelopment) {
+      if (process.platform === 'win32') {
+        process.on('message', data => {
+          if (data === 'graceful-exit') {
+            app.quit()
+          }
+        })
+      } else {
+        process.on('SIGTERM', () => {
+          app.quit()
+        })
+      }
+    }
 
     // 监听菜单状态改变
     ipcMain.on('control-toggle', (evt, args) => {
@@ -79,8 +119,6 @@ export default class {
       }
     })
 
-    this.ballwin.loadURL(this.winURL)
-
     this.ballwin.on('closed', () => {
       this.ballwin = null
     })
@@ -107,11 +145,21 @@ export default class {
       }
     })
 
-    this.controlwin.loadURL(`${this.winURL}/#/control`)
-
     this.controlwin.on('closed', () => {
       this.controlwin = null
     })
+
+    if (process.env.WEBPACK_DEV_SERVER_URL) {
+      // Load the url of the dev server if in development mode
+      this.ballwin.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+      this.controlwin.loadURL(`${process.env.WEBPACK_DEV_SERVER_URL}/control`)
+      if (!process.env.IS_TEST) this.ballwin.webContents.openDevTools()
+    } else {
+      createProtocol('app')
+      // Load the index.html when not in development
+      this.ballwin.loadURL('app://./index.html')
+      this.controlwin.loadURL(`app://./index.html/control`)
+    }
   }
 
   // 同步菜单位置
