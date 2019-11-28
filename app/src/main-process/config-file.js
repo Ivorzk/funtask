f68c62d4d63df3abdd22736a789eced233626456
ivorzk <ivorzk@qq.com>
2019-11-29 02:01:57 +0800

    更新主进程！

---
+++ app/src/main-process/config-file.js
@@ -0,0 +1,51 @@
+import YAML from 'yaml'
+import path from 'path'
+import os from 'os'
+import fs from 'fs'
+import gulp from 'gulp'
+import rename from 'gulp-rename'
+export default class {
+  constructor() {
+    // 加载配置文件
+    this.loadConfig()
+  }
+
+  // 获取配置文件目录
+  get configdir() {
+    return path.resolve(os.homedir() + '/.funtask')
+  }
+
+  // 检查配置文件是否存在
+  async getConfigFile() {
+    let file = ''
+    try {
+      file = fs.readFileSync(`${this.configdir}/config.yaml`, 'utf8')
+    } catch (e) {
+      // 复制默认配置文件至配置目录
+      gulp.src(path.resolve(`./static/default-config.yaml`))
+        .pipe(rename('config.yaml'))
+        .pipe(gulp.dest(this.configdir))
+      // 读取默认配置文件
+      file = fs.readFileSync(path.resolve(`./static/default-config.yaml`), 'utf8')
+    }
+    return file
+  }
+
+  // 载入配置文件
+  async loadConfig() {
+    // 尝试读取配置文件
+    let file = await this.getConfigFile()
+    // 解析配置文件并注入到全局变量中
+    global.$config = YAML.parse(file)
+    // 开始监听文件改变
+    this.watchConfigFile()
+  }
+
+  // 监听配置文件
+  watchConfigFile() {
+    gulp.watch(`${this.configdir}/config.yaml`, (event) => {
+      // 重新加载配置文件
+      this.loadConfig()
+    })
+  }
+}
---
+++ app/src/main-process/control.js
@@ -0,0 +1,130 @@
+'use strict'
+import {
+  app,
+  BrowserWindow,
+  ipcMain
+} from 'electron'
+/**
+ * 控制台
+ */
+export default class {
+  // 小球
+  ballwin = {}
+  // 菜单窗口
+  controlwin = {}
+  // 入口页面
+  winURL = process.env.NODE_ENV === 'development' ? `http://localhost:9080` : `file://${__dirname}/index.html`
+  // 构造函数
+  constructor() {
+    app.on('ready', () => {
+      this.createWindow()
+    })
+
+    app.on('window-all-closed', () => {
+      if (process.platform !== 'darwin') {
+        app.quit()
+      }
+    })
+
+    app.on('activate', () => {
+      if (this.ballwin === null) {
+        this.createWindow()
+      }
+    })
+
+    // 监听菜单状态改变
+    ipcMain.on('control-toggle', (evt, args) => {
+      // 切换窗体类型
+      let wintype = this.controlwin.getOpacity() === 0 ? 'control' : 'ball'
+      // 同步菜单位置
+      this.syncPosition(wintype)
+      this.ballwin.webContents.send('control-reply', wintype === 'ball')
+      this.controlwin.webContents.send('control-reply', wintype === 'control')
+      // 设置显示和隐藏 间隔300毫秒，等待动画执行完成
+      if (wintype === 'control') {
+        this.controlwin.setOpacity(wintype === 'control' ? 1 : 0)
+        setTimeout(() => {
+          this.ballwin.setOpacity(wintype === 'ball' ? 1 : 0)
+        }, 300)
+      } else {
+        this.ballwin.setOpacity(wintype === 'ball' ? 1 : 0)
+        setTimeout(() => {
+          this.controlwin.setOpacity(wintype === 'control' ? 1 : 0)
+        }, 300)
+      }
+    })
+  }
+
+  // 创建窗体
+  createWindow() {
+    /**
+     * Initial window options
+     */
+    this.ballwin = new BrowserWindow({
+      width: 88,
+      height: 88,
+      frame: false,
+      transparent: true,
+      resizable: false,
+      maximizable: false,
+      minimizable: false,
+      alwaysOnTop: true,
+      fullscreenable: false,
+      hasShadow: false,
+      useContentSize: true,
+      skipTaskbar: true,
+      webPreferences: {
+        nodeIntegration: true,
+        nodeIntegrationInWorker: true
+      }
+    })
+
+    this.ballwin.loadURL(this.winURL)
+
+    this.ballwin.on('closed', () => {
+      this.ballwin = null
+    })
+
+    // 初始化菜单
+    this.controlwin = new BrowserWindow({
+      x: 0,
+      y: 0,
+      width: 618,
+      height: 380,
+      frame: false,
+      transparent: true,
+      resizable: false,
+      maximizable: false,
+      minimizable: false,
+      alwaysOnTop: true,
+      fullscreenable: false,
+      hasShadow: true,
+      skipTaskbar: true,
+      opacity: 0,
+      webPreferences: {
+        nodeIntegration: true,
+        nodeIntegrationInWorker: true
+      }
+    })
+
+    this.controlwin.loadURL(`${this.winURL}/#/control`)
+
+    this.controlwin.on('closed', () => {
+      this.controlwin = null
+    })
+  }
+
+  // 同步菜单位置
+  syncPosition(flag) {
+    // 小球位置
+    let ballpos = this.ballwin.getPosition()
+    // 窗体位置
+    let controlpos = this.controlwin.getPosition()
+    let controlBounds = this.controlwin.getBounds()
+    if (flag === 'control') {
+      this.controlwin.setPosition(ballpos[0] - controlBounds.width + 75, ballpos[1] - controlBounds.height + 75)
+    } else {
+      this.ballwin.setPosition(controlpos[0] + controlBounds.width - 75, controlpos[1] + controlBounds.height - 75)
+    }
+  }
+}
