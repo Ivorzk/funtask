b255146af62397b561d669f157b68c59092cb42a
赵成围 <ivorzk@qq.com>
2019-11-28 16:01:20 +0800

    配置文件修改！

--- app/src/main/config-file.js
+++ app/src/main/config-file.js
@@ -2,7 +2,6 @@ import YAML from 'yaml'
 import path from 'path'
 import os from 'os'
 import fs from 'fs'
-import defaultConfig from './../../static/default-config.yaml'
 export default class {
   constructor() {
     // 加载配置文件
@@ -20,9 +19,9 @@ export default class {
     try {
       file = fs.readFileSync(`${this.configdir}/config.yaml`, 'utf8')
     } catch (e) {
-      console.log('文件读取异常')
-      // 创建一个默认的配置文件
-      console.log(defaultConfig, 'defaultConfig')
+      file = fs.readFileSync(path.resolve(`./static/default-config.yaml`), 'utf8')
+      // 复制默认配置文件至配置目录
+      fs.copyFileSync(path.resolve(`./static/default-config.yaml`), path.resolve(`${this.configdir}/config.yaml`))
     }
     return file
   }
@@ -30,8 +29,8 @@ export default class {
   // 载入配置文件
   async loadConfig() {
     // 尝试读取配置文件
-    let file = this.getConfigFile()
-    // 解析配置文件
-    YAML.parse(file)
+    let file = await this.getConfigFile()
+    // 解析配置文件并注入到全局变量中
+    global.$config = YAML.parse(file)
   }
 }
