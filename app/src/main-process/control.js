f9247bc30ddf3f38078d070188acf57c6e516475
ivorzk <ivorzk@qq.com>
2019-12-15 00:30:31 +0800

    细节调整！

--- app/src/main-process/control.js
+++ app/src/main-process/control.js
@@ -186,7 +186,6 @@ export default class {
       // Load the index.html when not in development
       this.ball.loadURL(`${global.$config.app.protocol}://./index.html#/`)
       this.control.loadURL(`${global.$config.app.protocol}://./index.html#/control`)
-      this.ball.webContents.openDevTools()
     }
   }
 
