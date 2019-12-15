a67a488e848f4ea36fa30474021eb638e8c749b1
ivorzk <ivorzk@qq.com>
2019-12-16 02:01:57 +0800

    同上！

--- app/src/main-process/control.js
+++ app/src/main-process/control.js
@@ -179,7 +179,7 @@ export default class {
     // 创建协议(主框架工作目录)
     // createProtocol(global.$config.app.protocol)
     // 创建app目录协议
-    if (!process.env.WEBPACK_DEV_SERVER_URL) {
+    if (process.env.WEBPACK_DEV_SERVER_URL) {
       // Load the url of the dev server if in development mode
       this.ball.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
       this.control.loadURL(`${process.env.WEBPACK_DEV_SERVER_URL}#/control`)
