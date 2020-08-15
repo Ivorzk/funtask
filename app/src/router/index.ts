f2c9bc5055c4d6815830dfffcaa74bfb77da7c73
ivorzk <ivorzk@qq.com>
2020-08-16 01:44:21 +0800

    同上！

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -11,7 +11,56 @@ const routes: Array<RouteConfig> = [{
   path: '/control',
   name: 'control',
   component: () => import('../views/control/index.vue'),
-  children: []
+  children: [{
+    path: '/funlist',
+    name: 'funlist',
+    component: () => import('../views/control/funlist.vue'),
+    meta: {
+      keepAlive: true
+    }
+  }, {
+    path: '/appstore',
+    name: 'appstore',
+    component: () => import('../views/control/appstore.vue'),
+    meta: {
+      keepAlive: true
+    }
+  }, {
+    path: '/feedback',
+    name: 'feedback',
+    component: () => import('../views/control/feedback.vue'),
+    meta: {
+      keepAlive: true
+    }
+  }, {
+    path: '/notice-list',
+    name: 'notice-list',
+    component: () => import('../views/notice/list.vue'),
+    meta: {
+      keepAlive: true
+    }
+  }, {
+    path: '/themes',
+    name: 'themes',
+    component: () => import('../views/control/themes.vue'),
+    meta: {
+      keepAlive: true
+    }
+  }, {
+    path: '/settings',
+    name: 'settings',
+    component: () => import('../views/control/settings.vue'),
+    meta: {
+      keepAlive: true
+    }
+  }]
+}, {
+  path: '/notice',
+  name: 'notice',
+  component: () => import('../views/notice/index.vue'),
+  meta: {
+    keepAlive: true
+  }
 }]
 
 const router = new VueRouter({
