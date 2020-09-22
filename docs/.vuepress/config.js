5933af46abe6bfa473b787eb615fca150926ef76
ivorzk <ivorzk@qq.com>
2020-09-22 10:11:33 +0800

    更新！

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -65,7 +65,7 @@ module.exports = {
       link: '/zh/vi'
     }, {
       text: '应用下载',
-      link: '/zh/introduction.html#Funtask客户端下载'
+      link: '/zh/introduction.html#funtask客户端下载'
     }, {
       text: '更新日志',
       link: '/zh/change-log'
