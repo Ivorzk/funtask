fb009c30f4497e07f3f39533eb251a66858390ad
ivorzk <ivorzk@qq.com>
2020-11-24 02:13:44 +0800

    更新

---
+++ docs/.vuepress/components/settled.vue
@@ -0,0 +1,31 @@
+<template>
+<section class="funtask-settled">
+  <dl>
+    <dt>
+      <img src="qrcode_wx.jpg"
+        alt="">
+    </dt>
+    <dd>扫码关注微信公众号</dd>
+  </dl>
+</section>
+</template>
+<script>
+
+</script>
+<style lang="scss">
+.funtask-settled {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: calc(100vh - 210px);
+
+    dl {
+        text-align: center;
+        dd,
+        dt {
+            margin: 0;
+            padding: 0;
+        }
+    }
+}
+</style>
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -77,7 +77,8 @@ module.exports = {
         // 'framework',
         'plugin',
         'api'
-      ]
+      ],
+      'settled': 'settled'
     }
   }
 }
