8ffa45756d5ee8b719dd9d5268b6e5f458bda868
ivorzk <ivorzk@qq.com>
2020-12-18 01:13:46 +0800

    首页改造！

---
+++ docs/.vuepress/components/social-contact.vue
@@ -0,0 +1,55 @@
+<template>
+<section class="funtask-social-contact">
+  <div class="container">
+    <a href="#">
+      <img src="icon_wx.png"
+        alt="">
+    </a>
+    <a href="#">
+      <img src="icon_sina.png"
+        alt="">
+    </a>
+    <a href="#">
+      <img src="icon_zhihu.png"
+        alt="">
+    </a>
+    <a href="#">
+      <img src="icon_linyin.png"
+        alt="">
+    </a>
+    <a href="#">
+      <img src="icon_toutiao.png"
+        alt="">
+    </a>
+  </div>
+</section>
+</template>
+<script>
+export default {
+  methods: {
+
+  }
+}
+</script>
+<style lang="scss">
+.funtask-social-contact {
+    padding: 1vw 0 5vw;
+    .container {
+        display: flex;
+        justify-content: flex-end;
+    }
+    a {
+        cursor: pointer;
+        display: inline-block;
+        margin: 0 10px;
+
+        &:last-child {
+            margin-right: 0;
+        }
+    }
+    img {
+        width: 39px;
+        pointer-events: none;
+    }
+}
+</style>
