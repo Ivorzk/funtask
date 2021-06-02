fd22cf297706639cca3bde253b3a1e3a8b3c9bf1
ivorzk <ivorzk@qq.com>
2021-06-02 18:48:48 +0800

    v

--- app/src/main-process/modules/updater.js
+++ app/src/main-process/modules/updater.js
@@ -1,8 +1,15 @@
-// 判断是否是生产环境
-if (!process.env.WEBPACK_DEV_SERVER_URL) {
-  require('update-electron-app')({
-    repo: 'https://github.com/Ivorzk/funtask',
-    updateInterval: '3 hour',
-    logger: require('electron-log')
-  })
+import {
+  autoUpdater
+} from 'electron'
+
+export default new class {
+  constructor() {
+    // 判断是否是生产环境
+    if (!process.env.WEBPACK_DEV_SERVER_URL) this.checkForUpdate()
+  }
+
+  checkForUpdate() {
+    console.log($config, 'autoUpdater ')
+    autoUpdater.setFeedURL($config.apihost + '/funtask/releases')
+  }
 }
