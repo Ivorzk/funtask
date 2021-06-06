77fed2067d8b9aaa989acd20c02ab18bf5978642
ivorzk <ivorzk@qq.com>
2021-06-07 01:31:25 +0800

    自动更新模块调整！

--- app/src/main-process/modules/updater.js
+++ app/src/main-process/modules/updater.js
@@ -1,40 +1,43 @@
 import {
-  autoUpdater,
-  dialog
-} from 'electron'
-
+  AppImageUpdater,
+  MacUpdater,
+  NsisUpdater
+} from 'electron-updater'
+const updateOptions = {
+  mac: {
+    requestHeaders: {
+      // Any request headers to include here
+      Authorization: 'Basic AUTH_CREDS_VALUE'
+    },
+    provider: 'generic',
+    url: 'https://example.com/auto-updates'
+  },
+  windows: {
+    requestHeaders: {
+      // Any request headers to include here
+      Authorization: 'Basic AUTH_CREDS_VALUE'
+    },
+    provider: 'generic',
+    url: 'https://example.com/auto-updates'
+  },
+  linux: {
+    requestHeaders: {
+      // Any request headers to include here
+      Authorization: 'Basic AUTH_CREDS_VALUE'
+    },
+    provider: 'generic',
+    url: 'https://example.com/auto-updates'
+  }
+}
 export default new class {
   constructor() {
-    // 设置更新地址
-    autoUpdater.setFeedURL('http://127.0.0.1:8360/funtask/releases')
-    // 监听事件
-    autoUpdater.on('checking-for-update', () => {
-      console.log('checking-for-update')
-    })
-    autoUpdater.on('update-available', () => {
-      console.log('update-available')
-    })
-    autoUpdater.on('update-not-available', () => {
-      dialog.showMessageBox({
-        title: '有更新'
-      })
-      console.log('update-not-available')
-    })
-    autoUpdater.on('update-downloaded', () => {
-      console.log('update-downloaded')
-    })
-    autoUpdater.on('before-quit-for-update', () => {
-      console.log('before-quit-for-update')
-    })
-    // 判断是否是生产环境
-    // if (!process.env.WEBPACK_DEV_SERVER_URL)
-
-    this.checkForUpdate()
-  }
-
-  checkForUpdate() {
-    // console.log($config, 'autoUpdater ')
-    // autoUpdater.setFeedURL($config.apihost + '/funtask/releases')
-    // autoUpdater.checkForUpdates()
+    if (process.platform === 'win32') {
+      autoUpdater = new NsisUpdater(updateOptions.windows)
+    } else if (process.platform === 'darwin') {
+      autoUpdater = new MacUpdater(updateOptions.mac)
+    } else {
+      autoUpdater = new AppImageUpdater(updateOptions.linux)
+    }
+    autoUpdater.checkForUpdatesAndNotify()
   }
 }
