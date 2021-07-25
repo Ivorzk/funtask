dfa5d08ad9153d97eeb2d42dab3ed206f551ed05
ivorzk <ivorzk@qq.com>
2021-07-26 01:45:24 +0800

    增加应用仓库自定义设置！

--- app/src/views/control/settings.vue
+++ app/src/views/control/settings.vue
@@ -1,18 +1,36 @@
 <template>
 <div class="funtask-settings">
   <mu-container>
-    <mu-form label-position="left" label-width="138" :model="form">
+    <mu-form label-position="left"
+      label-width="138"
+      :model="form">
       <mu-form-item label="开机自动启动">
         <mu-switch v-model="autostart"></mu-switch>
       </mu-form-item>
       <mu-form-item label="窗体固定外层">
         <mu-switch v-model="alwaysOnTop"></mu-switch>
       </mu-form-item>
+      <mu-form-item label="应用私有模式">
+        <mu-radio value="npm"
+          v-model="registryType"
+          label="npm"></mu-radio>
+        <mu-radio value="cnpm"
+          v-model="registryType"
+          label="cnpm"></mu-radio>
+        <mu-radio value="verdaccio"
+          v-model="registryType"
+          label="verdaccio"></mu-radio>
+      </mu-form-item>
+      <mu-form-item v-if="registryType=='verdaccio'"
+        label="应用镜像仓库">
+        <mu-text-field v-model="registry"></mu-text-field>
+      </mu-form-item>
     </mu-form>
   </mu-container>
 </div>
 </template>
 <script>
+import _ from 'lodash'
 export default {
   data() {
     return {
@@ -20,7 +38,11 @@ export default {
       // 自动启动
       autostart: false,
       // 窗口顶置
-      alwaysOnTop: false
+      alwaysOnTop: false,
+      // 镜像类型
+      registryType: 'npm',
+      // 镜像地址
+      registry: ''
     }
   },
   beforeMount() {
@@ -48,18 +70,40 @@ export default {
       this.$countly.$emit('system-setting-alwaysOnTop', {
         val
       })
-    }
+    },
+    registryType(val) {
+      this.$funtask.config.set({
+        app: {
+          registryType: val
+        }
+      })
+      this.$countly.$emit('system-setting-registryType', {
+        val
+      })
+    },
+    registry: _.debounce(function(val) {
+      this.$funtask.config.set({
+        app: {
+          registry: val
+        }
+      })
+      this.$countly.$emit('system-setting-registry', {
+        val
+      })
+    }, 600)
   },
   methods: {
     async getConfig() {
       const config = await this.$funtask.config.get()
       this.autostart = config.app.autostart
+      this.registry = config.app.registry
+      this.registryType = config.app.registryType
       this.alwaysOnTop = config.app.window.alwaysOnTop
     }
   }
 }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 .funtask-settings {
     display: flex;
     justify-content: center;
@@ -67,6 +111,9 @@ export default {
     color: $funtask-color-primary;
 
     .mu-form-item {
+        color: #fff;
+    }
+    .mu-radio-label {
         color: #fff;
     }
 }
