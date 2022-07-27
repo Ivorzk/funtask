c663341918d50077e9923849669ed9920f6d1475
ivorzk <ivorzk@qq.com>
2022-07-27 18:10:58 +0800

    v

--- app/src/main-process/tools/npm.js
+++ app/src/main-process/tools/npm.js
@@ -6,7 +6,6 @@ export default new class {
   // 搜索
   async search(keyword) {
     let res = await axios.get(`https://www.npmjs.com/search/suggestions?q=${keyword}`)
-    console.log(res, 'res')
     return res.data || []
   }
 }
